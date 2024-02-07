5988fb12e83ba9c8175a9426cae6524cb3513d55
Wallaa Kamal <wallaa85@gmail.com>
2024-02-07 02:30:52 -0800

    Change main

---
+++ lib/main.dart
@@ -0,0 +1,54 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    home: MyHomePage(),
+  ));
+}
+
+class MyHomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<MyHomePage> {
+  bool isVisible = true;
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Educative Answers'),
+        ),
+        body: Container(
+            child: Column(
+          children: <Widget>[
+            Visibility(
+              visible: isVisible,
+              child: Image.asset(
+                "assets/Educative.png",
+                height: 300,
+                fit: BoxFit.contain,
+              ),
+            ),
+            SizedBox(
+              height: 35,
+            ),
+            ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  textStyle: TextStyle(fontSize: 20),
+                  minimumSize: Size.fromHeight(50),
+                ),
+                onPressed: () {
+                  setState(() {
+                    isVisible = !isVisible;
+                  });
+                },
+                child: Text("Show/Hide")),
+          ],
+        )),
+      ),
+    );
+  }
+}
